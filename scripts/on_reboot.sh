28efd718d29d2f5ed4d8234fcc2d92125c0eabb8
zivhrog <zivh@rogat.co.il>
2018-10-11 08:56:53 +0300

    add on_reboot.sh

---
+++ scripts/on_reboot.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "Run Human Detection!"
+
+if pgrep -f "python dnnr.py" &>/dev/null; then
+    echo "it is already running"
+    exit
+fi
+source /home/pi/.profile
+workon cv
+cd /home/pi/vision
+python dnnr.py -i c -p "/dev/ttyAMA0" -b "38400" -m -l hd_log.log
