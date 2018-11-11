e2e1456b01f87e22734e0b5e8d420c9fa98343b0
zivhrog <zivh@rogat.co.il>
2018-11-11 09:36:28 +0200

    on_reboot script - add arguments info

--- scripts/on_reboot.sh
+++ scripts/on_reboot.sh
@@ -8,5 +8,11 @@ if pgrep -f "python dnnr.py" &>/dev/null; then
 fi
 source /home/pi/.profile
 workon cv
-cd /home/pi/vision
-python dnnr.py -i c -p "/dev/ttyAMA0" -b "115200" -m -l hd_log.log
+cd ~pi/vision
+python dnnr.py -i c -p "/dev/ttyAMA0" -b "115200" -m
+
+# Optional Arguments:
+#*********************
+# -v - optional argument - If added images will be saved to the Debug folder
+# -l "hd_log.log" - Write to a log file - need to add the file name. without this argument info will be written to the console
+# -d - Debug mode - Log level - debug. default is info.
